76d07a62fb26678e1718f25e005ee7d0d654fa4c
Joshua Jacobson <jmjpro@gmail.com>
2013-07-23 15:17:24 -0600

    see #178

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -1,5 +1,5 @@
 AudioPlayer.PRIORITY_CONFIG_PATH = 'js/audioPriority.json';
-function AudioPlayer(){
+function AudioPlayer(isEnabled){
 	var url = AudioPlayer.PRIORITY_CONFIG_PATH;
 	var audioPlayer = this;
 	Galapago.loadJsonAsync(url).then(function(audioPriority){
@@ -9,7 +9,32 @@ function AudioPlayer(){
 	this.currentAudio = null;
 	this.currentAudioInLoop = null;
 	this.loop = false;
+	this.isEnabled = isEnabled;
 }
+
+/* if audio is globally disabled we switch the definitions of AudioPlayer.play() and AudioPlayer.playInLoop() with these */
+//AudioPlayer.prototype.tempPlay = function(){};
+//AudioPlayer.prototype.tempPlayInLoop = function(){};
+
+AudioPlayer.prototype.disable = function() {
+	if( this.isEnabled ) {
+		this.isEnabled = false;
+		this.stop();
+		this.stopLoop();
+		AudioPlayer.prototype.tempPlay = this.play;
+		AudioPlayer.prototype.tempPlayInLoop = this.playInLoop;
+		this.play = function(){};
+		this.playInLoop = function(){};
+	}
+};
+
+AudioPlayer.prototype.enable = function() {
+	if( ! this.isEnabled ) {
+		this.isEnabled = true;
+		this.play = AudioPlayer.prototype.tempPlay;
+		this.playInLoop = AudioPlayer.prototype.tempPlayInLoop;
+	}
+};
 
 AudioPlayer.prototype.play = function(){
 	var audioPlayer = this;
@@ -27,7 +52,7 @@ AudioPlayer.prototype.play = function(){
 			}
 		}
 	}
-}
+};
 
 AudioPlayer.prototype.stop = function(){
 	this.queue.clear();
@@ -35,12 +60,12 @@ AudioPlayer.prototype.stop = function(){
 		this.currentAudio.pause();
 		this.currentAudio = null;
 	}
-}
+};
 
 AudioPlayer.prototype.playEnded = function(){
 	this.audioPlayer.currentAudio = null;
 	this.audioPlayer.play();
-}
+};
 
 AudioPlayer.prototype.playInLoop = function(key){
 	audioPlayer = this;
@@ -57,33 +82,33 @@ AudioPlayer.prototype.playInLoop = function(key){
 		this.currentAudioInLoop.loop = true;
 		this.currentAudioInLoop.play();
 	}
-}
+};
 
-AudioPlayer.prototype.stopLoop = function(key){
+AudioPlayer.prototype.stopLoop = function(){
 	if(this.currentAudioInLoop){
 		this.currentAudioInLoop.pause();
 	}
-}
+};
 
 
 AudioPlayer.prototype.playInvalidTileSelect = function(){
 	var key = "Cannot_select.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playTileSelect = function(){
 	var key = "Select.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playInvalidSwap = function(){
 	var index = Math.ceil( Math.random() * 5);
 	var key = "Piece_Off_0"+index+".mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playValidMatch = function(chainReactionCounter){
 	var index = (chainReactionCounter % 6) + 1;
@@ -91,7 +116,7 @@ AudioPlayer.prototype.playValidMatch = function(chainReactionCounter){
 	console.debug("playValidMatch: " + key );
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playGoldOrBlockingMatch = function(){
 	var key = "Combo1.mp3";
@@ -99,7 +124,7 @@ AudioPlayer.prototype.playGoldOrBlockingMatch = function(){
 	key = "Match_01.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playLightningStrike = function(){
 	var key = "Lightning.mp3";
@@ -107,31 +132,31 @@ AudioPlayer.prototype.playLightningStrike = function(){
 	key = "Match_01.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playCocoonMatch = function(){
 	var key = "Cocoon_Tile.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playSuperFriendMatch = function(){
 	var key = "SuperMatch.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playLevelWon = function(){
 	var key = "Win.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playGameOver = function(){
 	var key = "Win.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playTimeWarning = function(){
 	var key = "Warning.mp3";
@@ -145,57 +170,57 @@ AudioPlayer.prototype.playBonusFrenzyStart = function(){
 	var key = "SuperMatch.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playBonusFrenzySelect = function(){
 	var index = Math.ceil( Math.random() * 5);
 	var key = "Match_0"+index+".mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playFlipFlopSwap = function(){
 	var key = "Swap.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playFirePowerUsed = function(){
 	var key = "BurnIcon.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playShufflePowerUsed = function(){
 	var key = "Shuffle3.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playClick = function(){
 	var key = "Click.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playBounce = function(){
 	var key = "MahJong_PieceClack02.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
 
 AudioPlayer.prototype.playVolcanoLoop = function(){
 	var key = "Volcano_01.mp3";
 	this.playInLoop(key);
-}
+};
 
 AudioPlayer.prototype.playVolcanoEruptionLoop = function(){
 	var key = "Volcano_Eruption.mp3";
 	this.playInLoop(key);
-}
+};
 
 AudioPlayer.prototype.playReshuffle = function(){
 	var key = "Reshuffle.mp3";
 	this.queue.push(key, this.priorities[key]);
 	this.play();
-}
+};
