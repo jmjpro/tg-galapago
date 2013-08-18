b27c01b1369a211b35ba7d516b7a390341c0f419
Joshua Jacobson <jmjpro@gmail.com>
2013-08-18 14:01:32 +0300

    see #277

--- js/game.js
+++ js/game.js
@@ -1415,10 +1415,6 @@ Board.prototype.displayMenuButton = function(isActive) {
 		this.buttonActive = 'menuButton';
 		layer.drawImage(menuButtonImage, Level.MENU_BUTTON_X, Level.MENU_BUTTON_Y, menuButtonImage.width, menuButtonImage.height);
 		layer.drawImage(gameButtonHilight, Level.MENU_BUTTON_X - 1, Level.MENU_BUTTON_Y - 1, gameButtonHilight.width, gameButtonHilight.height);
-		/*
-		layer.strokeStyle = Tile.BORDER_COLOR_ACTIVE;
-		layer.strokeRect(Level.MENU_BUTTON_X, Level.MENU_BUTTON_Y, menuButtonImage.width, menuButtonImage.height);
-		*/
 	}
 	else {
 		this.buttonActive = null;
@@ -1439,10 +1435,6 @@ Board.prototype.displayQuitButton = function(isActive) {
 		this.buttonActive = 'menuButton';
 		layer.drawImage(quitButtonImage, quitImageX, quitImageY, quitButtonImage.width, quitButtonImage.height);
 		layer.drawImage(gameButtonHilight, quitImageX - 1, quitImageY - 1, gameButtonHilight.width, gameButtonHilight.height);
-		/*
-		layer.strokeStyle = Tile.BORDER_COLOR_ACTIVE;
-		layer.strokeRect(quitImageX, quitImageY, quitButtonImage.width, quitButtonImage.height);
-		*/
 	}
 	else {
 		this.buttonActive = null;
@@ -3173,13 +3165,10 @@ Tile has a matrix coordinate
 Tile can be Selected
 Tile can be MousedOver
 */
-Tile.CREATURE_BRIGHTNESS_ADJUSTMENT = 100;
 Tile.BORDER_COLOR = '#d3d3d3';
-Tile.BORDER_COLOR_ACTIVE = 'red';
 Tile.BORDER_WIDTH = 2;
-Tile.BORDER_RADIUS = 3;
-Tile.DELAY_AFTER_FLIP_MS = 250;
-Tile.DELAY_AFTER_ACTIVATE_MS = 10;
+Tile.DELAY_AFTER_FLIP_MS = 0;
+Tile.DELAY_AFTER_ACTIVATE_MS = 0;
 Tile.PLAIN_TILE_SPRITE_NUMBER = '0';
 Tile.CREATUREONLY_TILE_SPRITE_NUMBER = '1';
 Tile.BLOCKED_TILE_SPRITE_NUMBER = '2';
@@ -3229,7 +3218,6 @@ Tile.prototype.setActiveAsync = function(markTile) {
 	//console.debug('active tile ' + this.coordinates + ': ' + this.blob.creatureType);
 	deferred = Q.defer();
 	this.drawHilight();
-	//this.drawBorder(Tile.BORDER_COLOR_ACTIVE, Tile.BORDER_WIDTH);
 	this.board.level.levelAnimation.animateCreatureSelection(this.board.getLayer('CREATURE'), this.board, markTile);
 	Q.delay(Tile.DELAY_AFTER_ACTIVATE_MS).done(function() {
 		deferred.resolve();
@@ -3241,7 +3229,6 @@ Tile.prototype.setInactiveAsync = function() {
 	var deferred;
 	//console.debug('inactive tile ' + this.coordinates + ': ' + this.blob.creatureType);
 	deferred = Q.defer();
-	//this.drawBorder(Tile.BORDER_COLOR, Tile.BORDER_WIDTH);
 	this.eraseHilight();
 	Q.delay(Tile.DELAY_AFTER_ACTIVATE_MS).done(function() {
 		deferred.resolve();
@@ -3264,7 +3251,6 @@ Tile.prototype.setSelectedAsync = function() {
 Tile.prototype.setUnselected = function() {
 	this.board.gridLayer.clearRect( this.getXCoord(), this.getYCoord(), Board.TILE_WIDTH, Board.TILE_HEIGHT );
 	this.board.gridLayer.drawImage( this.board.level.gameImages.tile_1, this.getXCoord(), this.getYCoord(), Board.TILE_WIDTH, Board.TILE_HEIGHT );
-	//this.board.creatureLayer.drawImage( this.blob.image, this.getXCoord(), this.getYCoord(), Board.TILE_WIDTH, Board.TILE_HEIGHT );
 	return this; // chainable
 };
 
