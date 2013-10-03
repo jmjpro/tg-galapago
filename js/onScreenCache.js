ada7cbf92f10cbf599efd1feaf16c587f5a66351
Joshua Jacobson <jmjpro@gmail.com>
2013-10-03 15:14:59 +0300

    see #157

---
+++ js/onScreenCache.js
@@ -0,0 +1,168 @@
+/**
+ * @example
+ * 		var image1 = new Image(),
+ * 			image2 = document.createElement('img'),
+ * 			image1000 = document.getElementsByTagName('img')[0],
+ * 			evenArrayIsSupported = [ new Image(), new Image() ];
+ *
+ *      // Do this before any access to screen. Attention: consumes video memory (pixels).
+ * 		var mainMenuOnScreenCache = new OnScreenCache(
+ * 			[
+ * 				image1,
+ * 				image2,
+ *		 		image1000,
+ *		 		evenArrayIsSupported
+ * 			],
+ * 			function() {
+ * 		    	// do something after all images cached on screen
+ * 			}
+ * 		);
+ *
+ * 		// Do this BEFORE any current screen cleanup and BEFORE any other screen preparing/loading
+ * 		mainMenuOnScreenCache.destroy();
+ *
+ * @class OnScreenCache
+ * @param {Array.<Image|HTMLImageElement|Array.<Image|HTMLImageElement>>} imagesArray
+ * @param {function} [onCachedCallBack]
+ * @param {number} [timeOutInMilliSeconds]
+ * @constructor
+ */
+function OnScreenCache(imagesArray, onCachedCallBack, timeOutInMilliSeconds) {
+	var id = OnScreenCache.ID_PREFIX + OnScreenCache.nextId++,
+		element = document.createElement('div');
+
+	if(typeof timeOutInMilliSeconds === 'undefined') {
+		timeOutInMilliSeconds = 700;
+	}
+
+	element.id = id;
+	element.setAttribute('style', OnScreenCache.STYLE);
+	this.processImages(imagesArray, element);
+
+	document.body.appendChild(element);
+
+	/**
+	 * @private
+	 * @type {HTMLDivElement}
+	 */
+	this.cacheElement = element;
+
+	/**
+	 * @private
+	 * @type {string}
+	 */
+	this.id = id;
+
+	if(typeof onCachedCallBack !== 'undefined') {
+		//TODO: there is no way to determine that image is completely DRAWN on screen!
+		//TODO: currently this is just a timeout.
+		//TODO: NOTE: image LOADED is not the same as image DRAWN.
+		setTimeout(onCachedCallBack, timeOutInMilliSeconds);
+	}
+}
+
+/**
+ * @public
+ */
+OnScreenCache.prototype.destroy = function() {
+	var element = this.cacheElement;
+
+	// cacheElement can be detached from screen,
+	// so we can't find it by 'id', so we use reference to it
+	if(element) {
+		if(element.parentNode) {
+			this.removeImages(element);
+			element.parentNode.removeChild(element);
+		}
+		element.innerHTML = '';
+	}
+
+	// if someone did cacheElement.parentNode.innerHTML += 'someThing',
+	// reference this.cacheElement points to OLD instance of element,
+	// and innerHTML recreated new instance, so search by 'id'!
+	if(this.id !== null) {
+		element = document.getElementById(this.id);
+		if(element) {
+			if(element.parentNode) {
+				this.removeImages(element);
+				element.parentNode.removeChild(element);
+			}
+			element.innerHTML = '';
+		}
+	}
+
+	this.cacheElement = null;
+	this.id = null;
+};
+
+/**
+ * @static
+ * @private
+ * @type {string}
+ */
+OnScreenCache.STYLE =
+	'position: fixed; top: 0; left: 1279px; width: 1px; height: 1px;' +
+	'-webkit-background-size: 1px 1px;' +
+	'-o-background-size: 1px 1px;' +
+	'-moz-background-size: 1px 1px;' +
+	'-khtml-background-size: 1px 1px;' +
+	'-ms-background-size: 1px 1px;' +
+	'background-size: 1px 1px;';
+
+/**
+ * @static
+ * @private
+ * @type {string}
+ */
+OnScreenCache.ID_PREFIX = "on-screen-cache" + Date.now() + Math.random();
+
+/**
+ * @static
+ * @private
+ * @type {number}
+ */
+OnScreenCache.nextId = 0;
+
+/**
+ * @private
+ * @param {HTMLDivElement|Node} rootElement
+ * @param {Array.<Image|HTMLImageElement|Array.<Image|HTMLImageElement>>} imagesArray
+ */
+OnScreenCache.prototype.processImages = function(imagesArray, rootElement) {
+	// error protection
+	if(imagesArray) {
+		for(var i = imagesArray.length - 1; i >= 0; i--) {
+			var item = imagesArray[i];
+			// error protection
+			if(!item) {
+				continue;
+			}
+
+			// check if item is array
+			if(Object.prototype.toString.call( item ) === '[object Array]') {
+				this.processImages(item, rootElement);
+			} else {
+				var image = document.createElement('div');
+				image.setAttribute(
+					'style',
+					OnScreenCache.STYLE + "background-image: url('" + item.src + "'); background-repeat: no-repeat;"
+				);
+				rootElement.appendChild(image);
+			}
+		}
+	}
+};
+
+/**
+ * @private
+ * @param {HTMLDivElement|Node} rootElement
+ */
+OnScreenCache.prototype.removeImages = function(rootElement) {
+	if(rootElement) {
+		var el = rootElement.firstElementChild;
+		while(el) {
+			el.style.backgroundImage = '';
+			el = el.nextElementSibling;
+		}
+	}
+};
---
+++ js/store.js
@@ -0,0 +1,90 @@
+function Store(){
+}
+
+Store.prototype.localStoreSupport = function() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+};
+
+Store.prototype.setItem = function(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else {
+        var expires = "";
+    }
+    if( this.localStoreSupport() ) {
+        localStorage.setItem(name, value);
+    }
+    else {
+        document.cookie = name+"="+value+expires+"; path=/";
+    }
+};
+
+Store.prototype.getItem = function(name) {
+    if( this.localStoreSupport() ) {
+        return localStorage.getItem(name);
+    }
+    else {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+};
+
+Store.prototype.getKeys = function(name) {
+	var keys = [];
+    if( this.localStoreSupport() ) {
+        for (var i = 0; i < localStorage.length; i++){
+			keys.push(localStorage.key(i));
+		}
+    }
+    else {
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            var eqIndex = c.indexOf("=");
+            if (eqIndex > -1){ 
+            	keys.push(c.substring(0, eqIndex));
+            }
+        }
+    }
+    return keys;
+};
+ 
+Store.prototype.removeItem = function(name) {
+    if( this.localStoreSupport() ) {
+        localStorage.removeItem(name);
+    }
+    else {
+        this.setItem(name,"",-1);
+    }
+};
+
+Store.prototype.clear = function(){
+	if( this.localStoreSupport() ) {
+        localStorage.clear();
+    }
+    else {
+    	var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            var eqIndex = c.indexOf("=");
+            if (eqIndex > -1){ 
+            	this.setItem(c.substring(0, eqIndex), "", -1);
+            }
+        }
+    }	
+};
